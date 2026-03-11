106665ac2badd28b248df4ab11a4d62dd21e4d52
suja <wsjjsz@163.com>
2026-03-11 22:00:34 +0800

    chore: update web favicon links

--- api-frontend/config/config.ts
+++ api-frontend/config/config.ts
@@ -5,6 +5,11 @@ import proxy from './proxy';
 import routes from './routes';
 const { REACT_APP_ENV } = process.env;
 export default defineConfig({
+  links: [
+    { rel: 'icon', href: '/suapi_nav_logo_v2.svg' },
+    { rel: 'shortcut icon', href: '/suapi_nav_logo_v2.svg' },
+    { rel: 'apple-touch-icon', href: '/suapi_nav_logo_v2.svg' },
+  ],
   /**
    * @name 开启 hash 模式
    * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
